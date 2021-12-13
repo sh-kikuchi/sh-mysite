b28d9e0aad400b5c8602a223cbab61a22f26f145
sh-kikuchi <shu94hist726@gmail.com>
2021-12-13 21:42:47 +0900

    add_trip_toyama

--- mystudy/common/footer.php
+++ mystudy/common/footer.php
@@ -1,4 +1,4 @@
 <footer>
   <p class="footer-name">Time in History</p>
-  <div class="about-site"><a href="../about/policy.php">このサイトについて</a></div>
+  <!-- <div class="about-site"><a href="../about/policy.php">このサイトについて</a></div> -->
 </footer>
